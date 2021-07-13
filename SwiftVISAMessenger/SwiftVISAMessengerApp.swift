677c86f2c654de0431f07c9d115b4cb4ff7177db
seeaya <connorbarnesdenver@gmail.com>
2021-07-12 18:40:32 -0600

    Initial Commit

---
+++ SwiftVISAMessenger/SwiftVISAMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftVISAMessengerApp.swift
+//  SwiftVISAMessenger
+//
+//  Created by Connor Barnes on 7/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftVISAMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
